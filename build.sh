5b8da4f5e7553d1e903d256619fa384a49a8ae5d
NikoboiNFTB <github@nikoboi.dev>
2026-01-26 19:06:52 +0200

    update

--- build.sh
+++ build.sh
@@ -1,12 +1,36 @@
-desktop=$(xdg-user-dir DESKTOP)
-mkdir ~/wol/Guitar_Pro_8
-cp ~/wol/Downloads/Guitar_Pro_8/icon.png ~/wol/Downloads/Guitar_Pro_8/startup.sh ~/wol/Guitar_Pro_8
-cp ~/wol/Downloads/Guitar_Pro_8/guitarpro.desktop $desktop
-echo "Downloading Guitar Pro 8..."
-wget https://download-fr-3.guitar-pro.com/gp8/stable/guitar-pro-8-setup.exe
-echo "Preparing Wineprefix enviroment..."
-WINEARCH=win64 WINEPREFIX=~/wol/Guitar_Pro_8/ winetricks win7
-WINEARCH=win64 WINEPREFIX=~/wol/Guitar_Pro_8/ winetricks corefonts
-echo "Installing Guitar Pro 8..."
-WINEARCH=win64 WINEPREFIX=~/wol/Guitar_Pro_8/ wine guitar-pro-8-setup.exe
-echo "The installer have finished to work!"
+#!/usr/bin/env bash
+set -e
+
+REPO_DIR="$HOME/GitHub/NikoboiNFTB/Guitar-Pro-On-Linux"
+PREFIX="$HOME/wol/Guitar_Pro_8"
+DESKTOP="$HOME/Desktop"
+
+# Ensure directories exist
+mkdir -p "$PREFIX"
+
+# Copy startup script and icon
+cp "$REPO_DIR/startup.sh" "$PREFIX/"
+cp "$REPO_DIR/icon.png" "$PREFIX/"
+
+# Copy .desktop to desktop
+cp "$REPO_DIR/guitarpro.desktop" "$DESKTOP/"
+
+# Download Guitar Pro 8 installer
+cd "$PREFIX"
+if [ ! -f guitar-pro-8-setup.exe ]; then
+    echo "Downloading Guitar Pro 8..."
+    wget -O guitar-pro-8-setup.exe https://download-fr-3.guitar-pro.com/gp8/stable/guitar-pro-8-setup.exe
+fi
+
+# Prepare Wine prefix
+if [ ! -d "$PREFIX/drive_c" ]; then
+    echo "Setting up Wine prefix..."
+    WINEARCH=win64 WINEPREFIX="$PREFIX" wineboot --init
+    WINEARCH=win64 WINEPREFIX="$PREFIX" winetricks -q win7 corefonts
+fi
+
+# Install Guitar Pro 8
+echo "Launching Guitar Pro 8 installer..."
+WINEARCH=win64 WINEPREFIX="$PREFIX" wine "$PREFIX/guitar-pro-8-setup.exe"
+
+echo "Guitar Pro 8 setup finished!"
--- startup.sh
+++ startup.sh
@@ -1,1 +1,6 @@
-WINEARCH=win64 WINEPREFIX=~/wol/Guitar_Pro_8/ wine ~/wol/Guitar_Pro_8/drive_c/"Program Files"/"Arobas Music"/"Guitar Pro 8"/GuitarPro.exe
+#!/usr/bin/env bash
+set -e
+
+PREFIX="$HOME/wol/Guitar_Pro_8"
+
+WINEARCH=win64 WINEPREFIX="$PREFIX" wine "$PREFIX/drive_c/Program Files/Arobas Music/Guitar Pro 8/GuitarPro.exe"
